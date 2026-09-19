9fffdeef16b71e094bcb76fbbaa9471505887911
Cariad Eccleston <cariad@cariad.earth>
2026-09-19 17:21:56 +0100

    Ensure 100% test coverage across all operating systems

--- src/mosey/__init__.py
+++ src/mosey/__init__.py
@@ -5,3 +5,12 @@ from .step import Step
 __all__ = [
     "Step",
 ]
+
+
+# TODO: This function is intentionally not covered by any tests to check if CI detects
+# TODO: <100% coverage and prevents the change being merged.
+# TODO:
+# TODO: Delete this function after we observe CI fail.
+def temporary_uncovered_function() -> str:
+    """Return a greeting."""
+    return "Hello!"
