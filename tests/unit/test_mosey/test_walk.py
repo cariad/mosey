e90f783662cc06d738f7e1cdb03a7b3d6c30597d
Cariad Eccleston <cariad@cariad.earth>
2026-09-22 07:45:30 +0100

    Handle builds of Python without `os.geteuid`

--- tests/unit/test_mosey/test_walk.py
+++ tests/unit/test_mosey/test_walk.py
@@ -21,15 +21,27 @@ from tests.file_system_helpers import (
 )
 
 needs_posix_permissions = mark.skipif(
-    # The Windows check must come first because `os.geteuid` doesn't exist there.
-    sys.platform == "win32" or os.geteuid() == 0,
-    reason="`chmod` can't deny access on Windows or to the root user",
+    # Not every build of Python has `os.geteuid`, so we need to check for it.
+    #
+    # Two specific examples:
+    #
+    # - The WebAssembly build. That's fine; our `hasattr` check will catch that.
+    # - The Windows build. The `hasattr` check will catch that too, *but* that's not
+    #   enough to satisfy Pyright in a Windows development environment. We include the
+    #   `sys.platform == "win32"` check to shortcircuit Pyright outta there.
+    #
+    # If anyone wants to lint this package in a WebAssembly development environment...
+    # we'll cross that bridge when we come to it.
+    sys.platform == "win32" or not hasattr(os, "geteuid") or os.geteuid() == 0,
+    reason="`chmod` can't deny access on Windows, on WebAssembly, or to the root user",
 )
 """Skips a test that relies on `chmod` to deny access.
 
 Such a test can't be set up:
 
 - On Windows, where `chmod` can only toggle the read-only flag
+- On Python builds for WebAssembly, which have no user IDs and so no permissions to
+  deny.
 - As the root user, who bypasses permission checks.
 """
 
